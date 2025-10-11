7cba5525ba3d9ededce462fd8cd01f6862711411
Jason-427 <824767401@qq.com>
2025-10-11 20:05:09 +0800

    Updates

--- src/api/config.ts
+++
@@ -1,2 +0,0 @@
-export const SEVER_URL = "http://localhost:8000";
-// export const SEVER_URL = "https://llm.sending.network";
--- src/api/scenes.ts
+++
@@ -1,7 +0,0 @@
-import { request, buildUrl } from "./request";
-
-export const getScenes = () => {
-  return request(buildUrl("/scenes"), {
-    method: "GET",
-  });
-};
